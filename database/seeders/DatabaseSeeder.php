2f746584bae1f811885c21f07205cc932ff9b9f0
Čapčuch, Patrik - Student Rijn IJssel <00317034@rijnijssel.nl>
2024-10-03 13:17:53 +0200

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Enums\Role;
+use App\Models\Student;
+use App\Models\Teacher;
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Factories\StudentFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +18,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Teachers
+        User::factory(10)
+            ->create()
+            ->each(function ($user) {
+                Teacher::create([
+                    'user_id' => $user->id
+                ]);
+            });
 
+        // Students //
+        User::factory(10)
+            ->create(['role' => Role::Student])
+            ->each(function ($user) {
+                Student::factory()->create([
+                    'user_id' => $user->id
+                ]);
+            });
+
+        // Admins //
+        User::factory(4)->create(['role' => Role::Admin]);
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Test',
+            'last_name' => 'User',
             'email' => 'test@example.com',
+            'role' => Role::Admin
         ]);
     }
 }
