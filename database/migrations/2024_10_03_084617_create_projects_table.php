08bd52ea78bc3c4c4b16e3cda70a4111af71dc16
Patrik <00317034@rijnijssel.nl>
2024-10-03 15:54:40 +0200

    fix constraints

--- database/migrations/2024_10_03_084617_create_projects_table.php
+++ database/migrations/2024_10_03_084617_create_projects_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Student;
+use App\Models\Teacher;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -10,8 +12,8 @@ return new class extends Migration {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignIdFor(\App\Models\Student::class)->constrained();
-            $table->foreignIdFor(\App\Models\Teacher::class, 'assigned_id')->constrained();
+            $table->foreignIdFor(Student::class)->constrained('students', 'user_id');
+            $table->foreignIdFor(Teacher::class, 'assigned_id')->constrained('teachers', 'user_id');
             $table->timestamps();
         });
     }
