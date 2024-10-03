6566735dcd8c1a016dd0467bc03820fb9bc23fa4
David <00320666@rijnijssel.nl>
2024-10-03 12:36:59 +0200

    add admin role to migration

--- database/migrations/2024_10_03_083612_update_users_add_role.php
+++ database/migrations/2024_10_03_083612_update_users_add_role.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['Student', 'Teacher'])->default('Student');
+            $table->enum('role', ['Student', 'Teacher', 'Admin'])->default('Student');
             // Each user has the default role of student
             // This will be used for the polymorphic relationship
         });
